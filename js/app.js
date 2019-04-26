0c56d7efc290c4ba2108692b8e9739ddc41b07d1
beatrizsmerino <beatrizsmerino@gmail.com>
2019-04-27 01:30:50 +0200

    Added tasks with the loop for

--- js/app.js
+++ js/app.js
@@ -2,5 +2,19 @@ var app = new Vue({
     el: '#app',
     data: {
         title: "Things Todo",
+        tasks: [
+            {
+                title: "Do something awesome!",
+                status: false
+            },
+            {
+                title: "Buy toilet paper",
+                status: false
+            },
+            {
+                title: "Learn Vue",
+                status: false
+            }
+        ]
     }
 });
