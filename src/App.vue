ebb3b6419df8610ed1c8e4ace77b8208a28b6390
beatrizsmerino <beatrizsmerino@gmail.com>
2019-05-02 00:44:01 +0200

    Component vue-cli. Component TaskNew

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-	<page-title :title="title"></page-title>
+	<div id="app">
+		<page-title :title="title"></page-title>
+		<div class="todo-list">
+			<task-new :tasks="tasks"></task-new>
+		</div>
+	</div>
 </template>
 
 
 <script>
 	import PageTitle from "./components/ComponentTitle.vue";
+	import TaskNew from "./components/ComponentTaskNew.vue";
 
 	export default {
 		components: {
-			PageTitle
+			PageTitle,
+			TaskNew
 		},
 		data() {
 			return {
@@ -64,149 +71,8 @@
 		ul {
 			list-style: none;
 			padding: 25px;
-
-			li {
-				display: flex;
-				align-items: center;
-				line-height: 1;
-				user-select: none;
-
-				&:not(:last-child) {
-					padding-bottom: 15px;
-				}
-
-				&:hover {
-					cursor: text;
-
-					.task {
-						&__remove {
-							opacity: 1;
-							cursor: pointer;
-						}
-					}
-				}
-
-				&.is-done {
-					.task {
-						&__title {
-							color: #c3bbb6;
-
-							&:before {
-								width: 100%;
-							}
-						}
-					}
-				}
-			}
 		}
 	}
-
-	.task {
-		&__title {
-			position: relative;
-			transition: all 0.2s ease-in-out;
-
-			&:before {
-				transition: all 0.2s ease-in-out;
-				content: "";
-				height: 1px;
-				background: #c3bbb6;
-				width: 0%;
-				top: 50%;
-				position: absolute;
-				left: 0;
-			}
-		}
-
-		&__checkBox {
-			width: 20px;
-			height: 20px;
-			border: 2px solid #ddd4ce;
-			border-radius: 50%;
-			margin-right: 15px;
-			position: relative;
-			display: flex;
-			opacity: 0.7;
-			transition: all 0.2s ease-in-out;
-
-			&:hover {
-				cursor: pointer;
-				opacity: 1;
-			}
-
-			&:before {
-				transition: all 0.2s ease-in-out;
-				content: "\f00c";
-				font-family: "FontAwesome";
-				margin: auto;
-				font-size: 0.8em;
-				color: #31ecb8;
-				opacity: 0;
-				transform: scale(0.5);
-			}
-
-			&.is-done {
-				border-color: #31ecb8;
-				opacity: 1;
-
-				&:before {
-					opacity: 1;
-					transform: scale(1);
-				}
-			}
-		}
-		&__remove {
-			margin-left: auto;
-			padding-left: 15px;
-			opacity: 0;
-			color: #ff3c41;
-			transition: all 0.2s ease-in-out;
-			font-size: 1.2em;
-		}
-	}
-
-	.task-new {
-		background: rgba(0, 0, 0, 0.05);
-		padding: 15px;
-		display: flex;
-
-		&__input {
-			flex: 1;
-			margin-right: 10px;
-			padding: 5px 10px;
-			border: none;
-			outline: none;
-			border-left: 5px solid #f65050;
-			font-family: "Roboto", sans-serif;
-		}
-
-		&__button {
-			outline: none;
-			border: none;
-			border-radius: 50%;
-			font-size: 1.5em;
-			color: white;
-			background: #f65050;
-			display: flex;
-			width: 35px;
-			height: 35px;
-			transition: all 0.15s ease-in-out;
-
-			&:hover {
-				cursor: pointer;
-				background: #ff5c60;
-			}
-		}
-
-		&__icon{
-			width: 100%;
-			height: 100%;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-		}
-	}
-
 
 	/* Vue List Item transition */
 
---
+++ src/components/ComponentTaskNew.vue
@@ -0,0 +1,77 @@
+<template>
+  	<div class="task-new">
+		<input v-model="taskNew" @keyup.enter="taskAdd()" class="task-new__input" type="text" placeholder="New task">
+		<button class="task-new__button" @click="taskAdd()">
+			<span class="task-new__icon-add">+</span>
+		</button>
+  	</div>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				taskNew: ""
+			};
+		},
+		props: ["tasks"],
+		methods: {
+			taskAdd() {
+				const title = this.taskNew.trim();
+				if (title) {
+					this.tasks.push({
+						title: title,
+						status: false
+					});
+				}
+				this.taskNew = "";
+			}
+		}
+	};
+</script>
+
+
+<style lang="scss">
+	.task-new {
+		background-color: rgba(0, 0, 0, 0.05);
+		padding: 15px;
+		display: flex;
+
+		&__input {
+			flex: 1;
+			margin-right: 10px;
+			padding: 5px 10px;
+			border: none;
+			outline: none;
+			border-left: 5px solid #f65050;
+			font-family: "Roboto", sans-serif;
+		}
+
+		&__button {
+			outline: none;
+			border: none;
+			border-radius: 50%;
+			font-size: 1.5em;
+			color: white;
+			background: #f65050;
+			display: flex;
+			width: 35px;
+			height: 35px;
+			transition: all 0.15s ease-in-out;
+
+			&:hover {
+				cursor: pointer;
+				background: #ff5c60;
+			}
+		}
+
+		&__icon-add{
+			width: 100%;
+			height: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+</style>
