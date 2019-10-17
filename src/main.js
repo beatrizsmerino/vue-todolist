51031426d1cf8df60e91bbbd2dfc129eb14942e0
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2019-10-17 23:55:56 +0200

    Added icon for the Add task button. The icon was before a text and was not centered.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrash, faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faCheckCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faTrash, faCheckCircle)
+library.add(faTrash, faCheckCircle, faPlusCircle)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
