2fc26de5435ec834535ca89d9138abb377bee355
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-08-12 13:31:39 +0200

    VUEX. Created basic structure of 'state', 'getters', 'actions', 'mutations' and 'modules'

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+
+
 // LOAD VUEX
 Vue.use(Vuex)
 
+
+
+// STATE
+const state = {}
+
+
+
+// GETTERS
+const getters = {}
+
+
+
+// MUTACTIONS
+const mutations = {}
+
+
+
+// ACTIONS
+const actions = {}
+
+
+
+// MODULES
+const modules = {}
+
+
+
 // CREATE STORE
-const store = new Vuex.Store();
+const store = new Vuex.Store({
+	state,
+	getters,
+	mutations,
+	actions,
+	modules
+});
+
+
 
 export default { store };
