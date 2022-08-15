2d468889e1e403a53b620a1f6df1b7ca567565f3
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-08-15 22:38:43 +0200

    style(config): format code with the command `npm run lint`

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,7 @@
 import Vue from "vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEdit,
-	faTrash,
-	faCheckCircle,
-	faPlusCircle,
-	faTimes,
-	faInfo } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash, faCheckCircle, faPlusCircle, faTimes, faInfo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faEdit, faTrash, faCheckCircle, faPlusCircle, faTimes, faInfo);
