62a426da08ae2476abf6b6ca88e3f61e3d3fbdf8
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2022-09-12 04:04:13 +0200

    feat(icon): add package and configuration to create `Icon` component with sprite svg

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const path = require('path');
+const SVGSymbolSprite = require('svg-symbol-sprite-loader');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+// Define the resolve method to obtain the absolute path of the file
+const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
 
@@ -36,5 +41,36 @@ module.exports = {
 				files: ['src/**/*.{vue,scss}']
 			})
 		]
+	},
+
+	// Allows more fine-grained modifications to the internal webpack configuration
+	chainWebpack: config => {
+		// Create and insert sprite before the html body
+		config.
+			plugin('svg-symbol-sprite-loader').
+			after('html').
+			use(SVGSymbolSprite.Plugin).
+			end();
+
+		// Configure svg default rultes to exclude svg file proccessing in icons directory
+		config.module.
+			rule('svg').
+			exclude.add(resolve('src/assets/images/icons/svg')).
+			end();
+
+		// New icons rule, set svg sprite loader to process svg files in the 'src/assets/images/icons/svg' folder
+		config.module.
+			rule('svg-sprite').
+			uses.clear().
+			end().
+			test(/\.svg$/u).
+			include.add(resolve('src/assets/images/icons/svg')).
+			end().
+			use('svg-symbol-sprite-loader').
+			loader('svg-symbol-sprite-loader').
+			options({
+				symbolId: filePath => `icon-${path.basename(filePath, '.svg')}`
+			}).
+			end();
 	}
 };
